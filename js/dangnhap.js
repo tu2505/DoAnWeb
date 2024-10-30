04ed1976ad74f3b171aa4e214bca3f3c964e5642
Tuh <dotuan2k2@gmail.com>
2024-10-31 03:04:52 +0700

    Cap nhat dang nhap

--- js/dangnhap.js
+++ js/dangnhap.js
@@ -8,14 +8,19 @@ toggle = () => {
 setTimeout(() => {
 	container.classList.add('sign-in')
 }, 200)
+
 document.addEventListener('DOMContentLoaded', () => {
     let container = document.getElementById('container');
     let modal = document.getElementById('success-modal');
     let closeBtn = document.querySelector('.close-btn');
     let loginBtn = document.getElementById('login-btn'); // Assuming you have a login button
 
+    var userName=document.getElementById('signInUserName').value;
+    var password=document.getElementById('signInPass').value;
+
+    console.log(modal.querySelectorAll('.error-login'));
+
     modal.style.display='none'; 
-    console.log(loginBtn)
 
     // Hàm toggle giữa sign-in và sign-up
     toggle = () => {
@@ -23,15 +28,18 @@ document.addEventListener('DOMContentLoaded', () => {
         container.classList.toggle('sign-up');
     };
 
-    // Hiển thị modal sau khi đăng nhập thành công
-    function showSuccessModal(message) {
-        modal.querySelector('p').textContent = message; // Cập nhật thông báo
-        modal.style.display = 'block';
-    }
-
     // Đóng modal khi người dùng bấm nút "X"
     closeBtn.addEventListener('click', () => {
         modal.style.display = 'none';
+
+        //Chuyển hướng người dùng
+        
+        if(userName==='admin'&&password==='admin'){
+            
+        }else{
+
+        }
+
     });
 
     // Ẩn modal khi người dùng bấm bên ngoài modal
@@ -44,7 +52,19 @@ document.addEventListener('DOMContentLoaded', () => {
     // Hiển thị modal sau khi nhấn nút đăng nhập
     loginBtn.addEventListener('click', () => {
         container.classList.add('sign-in');
-        showSuccessModal('Đăng nhập thành công!');
+        if(userName==''&&password==''){
+            // modal.querySelectorAll('error-login').style.display='block';
+        }
+        else {
+            showSuccessModal('Đăng nhập thành công!');
+        }
+    
     });
 });
+
+// Hiển thị modal sau khi đăng nhập thành công
+function showSuccessModal(message) {
+    modal.querySelector('p').textContent = message; // Cập nhật thông báo
+    modal.style.display = 'block';
+}
 
