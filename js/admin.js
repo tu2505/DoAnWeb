02d6158c4cd28bb6f6441914f0c4bf23221a9aa5
EltonHuynh <tuanhuynh1478963@gmail.com>
2024-12-03 16:51:42 +0700

    update

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,18 @@
-function deleteProduct() {
-  document.querySelector(tr).classList.remove("active"),
-    (document.getElementById("delete").style.zIndex = "1");
+var data = [];
+
+function add() {
+  var item_name = document.getElementById("cart__product__item").value;
+  item_price = document.getElementById("cart__price").value;
+  item_quantity = document.getElementById("cart__quantity").value;
+
+  var item = {
+    Name: item_name,
+    price: item_price,
+    quantity: item_quantity,
+  };
+  data.push(item);
+}
+
+function deleteItem() {
+  document.getElementById("item").remove("active");
 }
